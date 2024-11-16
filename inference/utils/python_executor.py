28676b0b0ea675e36751be804906f9c151975ffb
WayXG <wexiong@google.com>
2024-11-16 11:51:06 -0600

    Update python_executor.py

--- inference/utils/python_executor.py
+++ inference/utils/python_executor.py
@@ -1,87 +1,104 @@
-# The evaluator is adapted from the ToRA project
-# https://github.com/microsoft/ToRA
-# ToRA authors: Zhibin Gou and Zhihong Shao and Yeyun Gong and yelong shen and Yujiu Yang and Minlie Huang and Nan Duan and Weizhu Chen
+import json
+import os
+import random
+from pathlib import Path
+from typing import Any, Iterable, Union
 
-import argparse
 import numpy as np
-from tqdm import tqdm
-from pebble import ProcessPool
-from concurrent.futures import TimeoutError
-
-from eval.grader import *
-from utils.parser import *
-from utils.utils import load_jsonl
-from utils.python_executor import PythonExecutor
-from datasets import load_dataset, Dataset
-
-def evaluate(data_name, output_dir=None):
-    if ".json" in data_name:
-        ds = load_dataset("json", data_files=data_name, split='train', field='instances').shuffle(seed=42)
-    else:
-        ds = load_dataset(data_name, split="train").shuffle(seed=42)
-
-    samples = [sample for sample in ds]
-    params = [(idx, pred, sample['gt']) for idx, sample in enumerate(samples) for pred in sample['pred']]
-
-    scores = []
-    timeout_cnt = 0 
-
-    with ProcessPool() as pool:
-        future = pool.map(math_equal_process, params, timeout=10)
-        iterator = future.result()
-        with tqdm(total=len(samples), desc="Evaluate") as progress_bar:
-            while True:
-                try:
-                    result = next(iterator)
-                    scores.append(result)
-                except StopIteration:
-                    break
-                except TimeoutError as error:
-                    print(error)
-                    scores.append(False)
-                    timeout_cnt += 1
-                except Exception as error:
-                    print(error.traceback)
-                    exit()
-                progress_bar.update(1) 
-
-    idx = 0
-    score_mat = []
-    for sample in samples:
-        sample['score'] = scores[idx: idx+len(sample['pred'])]
-        assert len(sample['score']) == len(sample['pred'])
-    
-  if ".json" in args.output_dir:
-      all_data = [sample for sample in samples]
-      output_eval_dataset = {}
-      output_eval_dataset["type"] = "text_only"
-      output_eval_dataset["instances"] = all_data
-      print("I collect ", len(all_data), "samples")
-      with open(output_dir, "w", encoding="utf8") as f:
-          json.dump(output_eval_dataset, f, ensure_ascii=False)
-  else:
-      all_data = [sample for sample in samples]
-      dict_data = {
-          "idx": [d['idx'] for d in all_data],
-          "gt": [d['gt'] for d in all_data],
-          "level": [d['level'] for d in all_data],
-          "type": [d['type'] for d in all_data],
-          "messages": [d['messages'] for d in all_data],
-          "pred": [d['pred'] for d in all_data],
-          "score": [d['score'] for d in all_data],
-      }
-
-      dataset = Dataset.from_dict(dict_data)
-      DatasetDict({'train': dataset}).push_to_hub(output_dir)
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--data_name", type=str, default="math")
-    parser.add_argument("--output_dir", type=str, default=None, required=True)
-    args = parser.parse_args()
-    return args
+def set_seed(seed: int = 42) -> None:
+    np.random.seed(seed)
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    print(f"Random seed set as {seed}")
 
-if __name__ == "__main__":
-    args = parse_args()
-    evaluate(data_name=args.data_name, output_dir=args.output_dir)
+
+def load_jsonl(file: Union[str, Path]) -> Iterable[Any]:
+    with open(file, "r", encoding="utf-8") as f:
+        for line in f:
+            try:
+                yield json.loads(line)
+            except:
+                print("Error in loading:", line)
+                exit()
+
+
+def save_jsonl(samples, save_path):
+    # ensure path
+    folder = os.path.dirname(save_path)
+    os.makedirs(folder, exist_ok=True)
+
+    with open(save_path, "w", encoding="utf-8") as f:
+        for sample in samples:
+            f.write(json.dumps(sample) + "\n")
+    print("Saved to", save_path)
+
+
+def lower_keys(example):
+    new_example = {}
+    for key, value in example.items():
+        if key != key.lower():
+            new_key = key.lower()
+            new_example[new_key] = value
+        else:
+            new_example[key] = value
+    return new_example
+
+
+def construct_prompt(args, example):
+    if args.prompt_type == "tora":
+        if "gemma" in args.model_name_or_path:
+            full_prompt = f"<bos><start_of_turn>user\n{example['question']}<end_of_turn>\n<start_of_turn>model\n"
+        elif "mistral" in args.model_name_or_path:
+            full_prompt = f"<s> [INST] {example['question']} [/INST]"
+        elif "deepseek" in args.model_name_or_path:
+            full_prompt = f"User: {example['question']}\nPlease integrate natural language reasoning with programs to solve the problem above, and put your final answer within \\boxed{{}}.\n\nAssistant: "
+        else:
+            raise NotImplementedError(args.prompt_type + "and " + args.model_name_or_path)
+    elif args.prompt_type == "cot":
+        if "gemma" in args.model_name_or_path:
+            full_prompt = f"<bos><start_of_turn>user\n{example['question']}\nPlease reason step by step.<end_of_turn>\n<start_of_turn>model\n"
+        elif "mistral" in args.model_name_or_path:
+            full_prompt = f"<s> [INST] {example['question']}\nPlease reason step by step, and put your final answer within \\boxed{{}}. [/INST]"
+        elif "deepseek" in args.model_name_or_path:
+            full_prompt = f"User: {example['question']}\nPlease reason step by step, and put your final answer within \\boxed{{}}.\n\nAssistant: "
+        elif "llama3" in args.model_name_or_path:
+            full_prompt = f"<|begin_of_text|><|start_header_id|>user<|end_header_id|>\n\n{example['question']}\nPlease reason step by step, and put your final answer within \\boxed{{}}.<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
+        else:
+            raise NotImplementedError(args.prompt_type + "and " + args.model_name_or_path)
+
+    return full_prompt
+
+
+key_map = {
+    "gt": "Ground Truth",
+    "pred": "Prediction",
+    "gt_cot": "Reference CoT",
+    "score": "Score",
+}
+
+
+def show_sample(sample, print_all_preds=False):
+    print("==" * 20)
+    for key in ["idx", "type", "level", "dataset"]:
+        if key in sample:
+            # capitalize
+            print("{}: {}".format(key[0].upper() + key[1:], sample[key]))
+    print("Question:", repr(sample["question"]))
+    if "code" in sample:
+        if print_all_preds:
+            for code in sample["code"]:
+                print("-" * 20)
+                print("code:", code)
+            print("Execution:", sample["report"])
+        else:
+            print("Solution:\n", sample["code"][0])
+            print("Execution:", sample["report"][0])
+    if "pred" in sample:
+        print("Prediction:", repr(sample["pred"][0]))
+    for key in ["gt", "score", "unit", "gt_cot"]:
+        if key in sample:
+            _key = key_map.get(key, key)
+            print("{}: {}".format(_key, repr(sample[key])))
+    print()
